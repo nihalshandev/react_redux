5041c502b0a7ee3d1adc21b750fb53ff21f9441a
nihalshanp <pnihalshan94@gmail.com>
2021-07-25 16:37:16 +0530

    Learned more about mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import CakeContainer from "./Components/CakeContainer";
-import HooksCakeContainer from "./Components/HooksCakeContainer";
-import IceCreamContainer from "./Components/IceCreamContainer";
-import LolipopContainer from "./Components/LolipopContainer";
-import NewCakeContainer from "./Components/NewCakeContainer";
+import ItemContainer from "./Components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-      <HooksCakeContainer />
-        <CakeContainer />
-        <IceCreamContainer/>
-        <LolipopContainer />
-        <NewCakeContainer />
+        <ItemContainer cake />
+        <ItemContainer />
       </div>
     </Provider>
   );
